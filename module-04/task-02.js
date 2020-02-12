fa0823325053a2e5d1cf3dbfb138c5c0be81ea72
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-02-12 17:55:59 +0200

    some changes

--- module-04/task-02.js
+++ module-04/task-02.js
@@ -1,23 +1,24 @@
 "use strict";
 
 const inventory = {
-    items: ["Монорельса", "Фильтр"],
+    items: ['Knife', 'Gas mask'],
     add(itemName) {
+        console.log(`Adding ${itemName} to inventory`);
         this.items.push(itemName);
     },
     remove(itemName) {
+        console.log(`Removing ${itemName} from inventory`);
         this.items = this.items.filter(item => item !== itemName);
-    }
+    },
 };
 
-const invokeInventoryOperation = function (object, inventoryAction, itemName) {
-    console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
-    const action = inventoryAction.bind(object, itemName);
-    action();
+const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
 };
 
-invokeInventoryOperation(inventory, inventory.add, 'Аптечка');
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 console.log(inventory.items);
 
-invokeInventoryOperation(inventory, inventory.remove, 'Фильтр');
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 console.log(inventory.items);
